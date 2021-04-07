2d20ed2f32ff38fa43e1c59f180898fc07c8fb39
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-04-06 22:24:14 -0300

    Route collection with MongoDB (mongoose)

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -f ".env" ]; then
+    cp .env.sample .env
+fi
+
 npm install
 
 npm run start:dev
--- src/routes/entities/route.entity.ts
+++ src/routes/entities/route.entity.ts
@@ -1,1 +1,27 @@
-export class Route {}
+import { Prop, Schema, raw, SchemaFactory } from '@nestjs/mongoose';
+
+export type RouteDocument = Route & Document;
+
+@Schema()
+export class Route {
+  @Prop()
+  title: string;
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  startPosition: { lat: number; lng: number };
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  endPosition: { lat: number; lng: number };
+}
+
+export const RouteSchema = SchemaFactory.createForClass(Route);
